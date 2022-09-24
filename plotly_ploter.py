d78c4350158fc1f03a006efec2ef5f8bc50e0b2f
Eli Countrywood <countrywoodster@gmail.com>
2022-09-24 10:44:29 -0700

    add plotly_plotter

---
+++ plotly_ploter.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+import plotly.graph_objs as go
+
+''' 
+Class to create a line plot
+
+Use:
+import plotly_ploter as pp
+plotter = pp.Plotly_Plotter()
+plotter.plot(xvals, yvals)
+
+'''
+
+class Plotly_Plotter:
+
+    def __init__(self) -> None:
+        self.xvals = None
+        self.yvals = None
+
+    def plot(self, xvals: np.ndarray, yvals:np.ndarray):
+        self.xvals = xvals
+        self.yvals = yvals
+        
+        trace1 = go.Scatter(
+                        name = "trace 1",
+                        x = self.xvals,
+                        y = self.yvals,
+                        mode='lines',
+                        marker = dict(color = 'rgb(102,255,255)'),
+                        text = self.xvals)
+
+        data = [trace1]
+        layout = go.Layout(title = 'Line Plot',
+                    xaxis= dict(title= 'xval',ticklen= 5,zeroline= False),
+                    yaxis= dict(title= 'yval',ticklen= 5,zeroline= False)
+                    )
+
+        fig = go.Figure(data = data, layout = layout)
+        fig.show()
+        fig.write_html("plot.html")
